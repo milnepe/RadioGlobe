cc5c8bb92fd5cf8a8b759e3d99f0cdfda0a8bc87
Peter Milne <milnepe@gmail.com>
2024-05-08 18:51:54 +0100

    Increase default fuzziness

--- radio_config.py
+++ radio_config.py
@@ -14,7 +14,7 @@ OFFSETS_JSON = "data/offsets.json"
 
 # Higher values of fuzziness increases the search area.
 # May include more than one city may be included if they are located close together.
-FUZZINESS = 2
+FUZZINESS = 3
 
 # Affects ability to latch on to cities
 STICKINESS = 3
