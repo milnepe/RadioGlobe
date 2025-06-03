9af16f5702a8647ba2aaeb13ed396972143b5ea4
Peter Milne <milnepe@gmail.com>
2025-06-03 16:36:00 +0100

    Update python installation

--- install.sh
+++ install.sh
@@ -1,39 +1,79 @@
 #! /usr/bin/bash
 
-# Install all dependencies and setup radioglobe service to run under default user
+# Install all dependencies and setup radioglobe service to run under radioglobe user
+# The script assumes the repo is cloned to the radioglobe home directory
+# and it is executed from within the ~/RadioGlobe dir
 
-sudo apt install vlc-bin vlc-plugin-base python3-venv python3-dev pulseaudio-module-bluetooth
+# sudo apt install vlc-bin vlc-plugin-base python3-venv python3-dev pulseaudio-module-bluetooth
 # sudo apt install vlc pulseaudio python3-pip python3-smbus python3-dev python3-rpi.gpio
+echo "Installing OS dependencies..."
+sudo apt install python3-dev pulseaudio-module-bluetooth
 
 # Create python virtual environment and activate it so python packages can be installed in it
 echo "Creating virtual environment..."
-python -m venv venv
-source ./venv/bin/activate
+export RADIOGLOBE_DIR=/opt/radioglobe
+sudo mkdir $RADIOGLOBE_DIR
+sudo chown radioglobe:radioglobe $RADIOGLOBE_DIR
+python -m venv $RADIOGLOBE_DIR/venv
+source $RADIOGLOBE_DIR/venv/bin/activate
+cp RadioGlobe/requirements.txt $RADIOGLOBE_DIR/
+pip install -r $RADIOGLOBE_DIR/requirements.txt
 
+# Check python dependencies are correctly installed in venv
+echo "Checking python venv..."
+if [[ -z "$VIRTUAL_ENV" ]]; then
+  echo "❌ Please activate your virtual environment first."
+  exit 1
+fi
+
+echo "🔍 Checking Python packages in requirements.txt against current venv..."
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+  [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
+
+  # Extract package name
+  if [[ "$line" =~ @\  ]]; then
+    # Handle VCS entries like: package @ git+https://...
+    pkg_name=$(echo "$line" | cut -d '@' -f 1 | xargs)
+  else
+    # Handle regular entries like: package==version
+    pkg_name=$(echo "$line" | cut -d '=' -f 1 | cut -d '<' -f 1 | cut -d '>' -f 1 | xargs)
+  fi
+
+  # Check if the package is installed
+  pip show "$pkg_name" > /dev/null 2>&1
+  if [[ $? -eq 0 ]]; then
+    echo "✅ $pkg_name is installed"
+  else
+    echo "❌ $pkg_name is NOT installed"
+  fi
+done < $RADIOGLOBE_DIR/requirements.txt
+
+# Replaced by above to put dependencies into venv not OS - Bullseye no longer supported
 # Install python dependencies
-pip install spidev
-pip install smbus
-pip install python-vlc
-pip install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
+# pip install spidev
+# pip install smbus
+# pip install python-vlc
+# pip install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 
 # Install appropriate GPIO support
-source /etc/os-release
-echo "$VERSION_CODENAME"
-case $VERSION_CODENAME in
-    bullseye)
-    # Legacy support
-    pip install RPi.GPIO
-    ;;
-    bookworm)
-    # Bookworm compatibility with RPi.GPIO
-    pip install lgpio
-    pip install rpi-lgpio
-    ;;
-    *)
-    echo "Debian version unknown"
-    exit
-    ;;
-esac
+# source /etc/os-release
+# echo "$VERSION_CODENAME"
+# case $VERSION_CODENAME in
+#     bullseye)
+#     # Legacy support
+#     pip install RPi.GPIO
+#     ;;
+#     bookworm)
+#     # Bookworm compatibility with RPi.GPIO
+#     pip install lgpio
+#     pip install rpi-lgpio
+#     ;;
+#     *)
+#     echo "Debian version unknown"
+#     exit
+#     ;;
+# esac
 
 # Remove any old radioglobe service
 FILE=/etc/systemd/system/radioglobe.service
