7737c6f01ca6a2fa09679ee0b19c914148dc518a
Peter Milne <milnepe@gmail.com>
2025-07-06 08:14:20 +0100

    Setup logging

--- radioglobe/main.py
+++ radioglobe/main.py
@@ -242,8 +242,7 @@ class App:
 
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
 
     logging.info("Starting RadioGlobe...")
