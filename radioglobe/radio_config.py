46a6f4f41c4689ef84fb4b98e03b272164eee0f4
Peter Milne <milnepe@gmail.com>
2026-04-14 19:42:02 +0100

    Increase default fuzziness and stickiness

--- radioglobe/radio_config.py
+++ radioglobe/radio_config.py
@@ -17,10 +17,10 @@ ENCODER_RESOLUTION = 1024
 
 # Higher values of fuzziness increases the search area.
 # May include more than one city may be included if they are located close together.
-FUZZINESS = 2
+FUZZINESS = 3
 
 # Affects ability to latch on to cities
-STICKINESS = 3
+STICKINESS = 10
 
 # Edit these to suit your audio settings
 VOLUME_STEP = 10
