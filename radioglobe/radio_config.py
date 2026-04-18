a12760429c9a7068efd4c4a40e2ab35fefb3d3a4
Peter Milne <milnepe@gmail.com>
2026-04-18 09:05:24 +0100

    Reduce default fuzziness

--- radioglobe/radio_config.py
+++ radioglobe/radio_config.py
@@ -6,7 +6,7 @@ ENCODER_RESOLUTION = 1024
 
 # Higher values of fuzziness increases the search area.
 # May include more than one city may be included if they are located close together.
-FUZZINESS = 5
+FUZZINESS = 3
 
 # Affects ability to latch on to cities
 STICKINESS = 2
